2ee3f5f56212360abbc5a24d126fb06eaa9f053a
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-06-25 10:50:48 +0530

    Initial commit

---
+++ src/main/java/Collections/Iterators/SetIteratorExample.java
@@ -0,0 +1,32 @@
+package Collections.Iterators;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetIteratorExample {
+    public static void main(String[] args) {
+        // Create a set of strings
+        Set<String> set = new HashSet<>();
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Cherry");
+        set.add("Date");
+        set.add("Elderberry");
+
+        // Obtain an Iterator
+        Iterator<String> iterator = set.iterator();
+
+        // Traverse the set
+        System.out.println("Traversing the set:"); // Output: Traversing the set:
+        while (iterator.hasNext()) {
+            System.out.println("Element: " + iterator.next());
+        }
+        // Output:
+//        Element: Apple
+//        Element: Cherry
+//        Element: Date
+//        Element: Elderberry
+//        Element: Banana
+    }
+}
